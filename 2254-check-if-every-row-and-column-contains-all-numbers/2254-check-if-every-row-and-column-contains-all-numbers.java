74918994a91db42ab3dd4377967ddd5daf7a26be
Prakash-M-S <168627798+Prakash-M-S@users.noreply.github.com>
2025-02-08 10:47:56 +0530

    Time: 5 ms (84.71%), Space: 45.4 MB (70.45%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,25 +1,20 @@
 class Solution {
     public boolean checkValid(int[][] matrix) {
-        int R = matrix.length;
-        int C = matrix[0].length;
+        int n = matrix.length;
 
-        HashSet<Integer>[] rowSet = new HashSet[R];
-        HashSet<Integer>[] colSet = new HashSet[C];
+        for (int i = 0; i < n; i++) {
+            boolean[] rowCheck = new boolean[n];
+            boolean[] colCheck = new boolean[n];
 
-        for (int i = 0; i < R; i++) {
-            rowSet[i] = new HashSet<>();
-            colSet[i] = new HashSet<>();
-        }
+            for (int j = 0; j < n; j++) {
+                int rowVal = matrix[i][j] - 1;
+                int colVal = matrix[j][i] - 1;
 
-        for (int row = 0; row < R; row++) {
-            for (int col = 0; col < C; col++) {
-                int dig = matrix[row][col];
-
-                if (dig < 1 || dig > R || rowSet[row].contains(dig) || colSet[col].contains(dig)) {
+                if (rowCheck[rowVal] || colCheck[colVal]) {
                     return false;
                 }
-                rowSet[row].add(dig);
-                colSet[col].add(dig);
+                rowCheck[rowVal] = true;
+                colCheck[colVal] = true;
             }
         }
         return true;
