e0632a22ba191de929e2d2ec7011b7f08207679f
Prakash-M-S <168627798+Prakash-M-S@users.noreply.github.com>
2025-02-26 20:05:17 +0530

    Time: 24 ms (94.59%), Space: 55.7 MB (20.08%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,11 +1,29 @@
+class Point{
+    int p,s;
+    Point(int p,int s)
+    {
+        this.p=p;
+        this.s=s;
+    }
+}
 class StockSpanner {
-    S
+ Deque<Point> stack;
     public StockSpanner() {
+        stack= new ArrayDeque<>();
+
+
+
         
     }
     
     public int next(int price) {
-        
+        int span=1;
+        while(!stack.isEmpty()&&stack.peek().p<=price)
+        {
+            span+=stack.pop().s;
+        }
+        stack.push(new Point(price,span));
+        return span;
     }
 }
 
