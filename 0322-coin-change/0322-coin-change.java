240b6ab2a3ec15caeda92276170ae0884768787d
Prakash-M-S <168627798+Prakash-M-S@users.noreply.github.com>
2025-03-14 17:35:36 +0530

    Time: 10 ms (99.36%), Space: 44.2 MB (88.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,33 +1,17 @@
 class Solution {
-    int recursion(int i,int[] coins,int amount,int dp[][])
-    {
-        if(i==0)
-        {
-            if(amount%coins[0]==0)
-            {
-                return amount/coins[0];
-            }
-            return (int)1e9;
-        }
-        if(dp[i][amount]!=-1)
-        {
-            return dp[i][amount];
-        }
-        int not_take=recursion(i-1,coins,amount,dp);
-        int take=coins[i]<=amount?1+recursion(i,coins,amount-coins[i],dp):(int)1e9;
-        
-        
-        return dp[i][amount]=Math.min(not_take,take);
-
-    }
     public int coinChange(int[] coins, int amount) {
           int n=coins.length;
-          int dp[][]=new int[n+1][amount+1];
-          for(int[] d:dp)
+          int dp[]=new int[amount+1];
+          Arrays.fill(dp,(int)1e9);
+          dp[0]=0;
+          for(int coin:coins)
           {
-        Arrays.fill(d,-1);
+            for(int j=coin;j<=amount;j++)
+            {
+                dp[j]=Math.min(dp[j],1+dp[j-coin]);
+            }
           }
-          int ans=recursion(coins.length-1,coins,amount,dp);
+          int ans=dp[amount];
           if(ans<1e9)
           {
             return ans;
