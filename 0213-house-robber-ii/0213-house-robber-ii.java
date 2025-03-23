447ec12f060fad421dffa492686683cd404bb6d6
Prakash-M-S <168627798+Prakash-M-S@users.noreply.github.com>
2025-03-23 12:46:53 +0530

    Time: 0 ms (100%), Space: 41.2 MB (49.37%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,8 +1,6 @@
 class Solution {
     public int rob(int[] arr) {
         int n=arr.length;
-        int dp[]=new int[n];
-        int dp2[]=new int[n];
         if(n==1)
         {
             return arr[0];
@@ -11,18 +9,20 @@ class Solution {
         {
             return Math.max(arr[0],arr[1]);
         }
-        dp[0]=arr[0];
-        dp[1]=Math.max(arr[0],arr[1]);
-        for(int i=2;i<n-1;i++)
+        int first = helper(arr,0,n-1);
+        int second = helper(arr,1,n);
+        return Math.max(first,second);
+    }
+    public int helper(int[] arr,int start,int end)
+    {
+        int p2=0;
+        int p1=0;
+        for(int i=start;i<end;i++)
         {
-            dp[i]=Math.max(dp[i-1],arr[i]+dp[i-2]);
+            int curr=Math.max(p1,arr[i]+p2);
+            p2=p1;
+            p1=curr;
         }
-        dp2[1]=arr[1];
-        dp2[2]=Math.max(arr[1],arr[2]);
-        for(int i=3;i<n;i++)
-        {
-            dp2[i]=Math.max(dp2[i-1],arr[i]+dp2[i-2]);
-        }
-        return Math.max(dp[n-2],dp2[n-1]);
+        return p1;
     }
 }
