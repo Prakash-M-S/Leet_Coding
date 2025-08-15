61521d956bd1c38e5e12313e0c9ce094f7ab9e2c
Prakash-M-S <168627798+Prakash-M-S@users.noreply.github.com>
2025-08-15 18:21:38 +0530

    Time: 26 ms (37.74%), Space: 45.8 MB (97.51%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -7,10 +7,7 @@ class Solution {
         for(int i=0;i<nums.length;i++)
         {
             running+=nums[i];
-            if(map.containsKey(running-k))
-            {
-                count+=map.get(running-k);
-            }
+            count += map.getOrDefault(running - k, 0);
             map.put(running,map.getOrDefault(running,0)+1);
         }
         return count;
