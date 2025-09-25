77f86cc4b7d0a22c43d08678c66f506ded294aef
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-09-25 22:23:03 +0530

    Time: 0 ms (100%), Space: 40.7 MB (27.29%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,23 +1,38 @@
 class Solution {
-    int dp[];
-    private int recurse(int n)
-    {
-        if(n<=1){
+    // private int recurse(int n)
+    // {
+    //     if(n<=1){
+    //         return n;
+    //     }
+    //     if(n==2)
+    //     {
+    //         return 1;
+    //     }
+    //     if(dp[n]!=0)
+    //     {
+    //         return dp[n];
+    //     }
+    //     dp[n]= recurse(n-1)+recurse(n-2)+recurse(n-3);
+    //     return dp[n];
+    // }
+    public int tribonacci(int n) {
+        if(n<=1)
+        {
             return n;
         }
         if(n==2)
         {
             return 1;
         }
-        if(dp[n]!=0)
+        int dp[]=new int[n+1];
+
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++)
         {
-            return dp[n];
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
-        dp[n]= recurse(n-1)+recurse(n-2)+recurse(n-3);
         return dp[n];
-    }
-    public int tribonacci(int n) {
-        dp=new int[n+1];
-        return recurse(n);
     }
 }
