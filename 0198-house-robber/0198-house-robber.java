da6929bb3022312f645aa922fdded0eba93f04ca
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-09-26 11:01:28 +0530

    Time: 0 ms (100%), Space: 41.2 MB (35.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,37 @@
 class Solution {
-    static int houses[];
-    int house(int nums[],int n,int position)
-    {
-        if(position>=n)
+    // static int houses[];
+    // int house(int nums[],int n,int position)
+    // {
+    //     if(position>=n)
+    //     {
+    //         return 0;
+    //     }
+    //     if(houses[position]!=-1)
+    //     {
+    //         return houses[position];
+    //     }
+    //     int take=nums[position]+house(nums,n,position+2);
+    //      int not_take=0+house(nums,n,position+1);
+    //      houses[position]=Math.max(take,not_take);
+    //      return houses[position];
+    // }
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[] houses=new int[n];
+        if(n==0)
         {
             return 0;
         }
-        if(houses[position]!=-1)
+        if(n==1)
         {
-            return houses[position];
+            return nums[0];
         }
-        int take=nums[position]+house(nums,n,position+2);
-         int not_take=0+house(nums,n,position+1);
-         houses[position]=Math.max(take,not_take);
-         return houses[position];
-    }
-    public int rob(int[] nums) {
-        int n=nums.length;
-        houses=new int[n];
-        Arrays.fill(houses,-1);
-        return house(nums,n,0);
+        houses[0]=nums[0];
+        houses[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            houses[i]=Math.max(nums[i]+houses[i-2],houses[i-1]);
+        }
+        return Math.max(houses[n-1],houses[n-2]);
     }
 }
