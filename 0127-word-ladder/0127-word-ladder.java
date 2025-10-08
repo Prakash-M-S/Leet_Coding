6166a9526f17b86ae213575330540438ba63e22d
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-10-08 21:17:02 +0530

    Time: 47 ms (90.8%), Space: 51.8 MB (11.75%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,44 +1,59 @@
-class word{
+
+class Solution {
+    class Word{
     String s=" ";
     int step=0;
-    word(String st,int steps)
+    Word(String st,int steps)
     {
         s=st;
         step=steps;
     }
 }
-class Solution {
-    public boolean one(String s1,String s2)
-    {
-        int diff = 0;
-    for (int i = 0; i < s1.length(); i++) {
-        if (s1.charAt(i) != s2.charAt(i)) {
-            diff++;
-            if (diff > 1) return false;
-        }
-    }
-    return diff == 1;
-    }
+    // public boolean one(String s1,String s2)
+    // {
+    //     int diff = 0;
+    // for (int i = 0; i < s1.length(); i++) {
+    //     if (s1.charAt(i) != s2.charAt(i)) {
+    //         diff++;
+    //         if (diff > 1) return false;
+    //     }
+    // }
+    // return diff == 1;
+    // }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         int len=wordList.size();
         if(!wordList.contains(endWord)) return 0;
-        boolean visited[]=new boolean[len];
-        Queue<word> que=new LinkedList<>();
-        que.offer(new word(beginWord,1));
+        Queue<Word> que=new LinkedList<>();
+        Map<String,List<String>> map=new HashMap<>();
+        for(String next:wordList)
+        {
+            for(int i=0;i<beginWord.length();i++)
+            {
+                String pattern = next.substring(0,i)+'*'+next.substring(i+1);
+                map.computeIfAbsent(pattern,k->new ArrayList<>()).add(next);
+            }
+        }
+        Set<String> visited = new HashSet<>();
+        que.offer(new Word(beginWord,1));
+        visited.add(beginWord);
         while(!que.isEmpty())
         {
-            word curr=que.poll();
+            Word curr=que.poll();
             String a=curr.s;
             int b=curr.step;
-            for(int i=0;i<len;i++)
-            {
-                if(!visited[i]&&one(a,wordList.get(i)))
+                for(int i =0 ;i<a.length();i++)
                 {
-                    if(wordList.get(i).equals(endWord)) return b+1;
-                    visited[i]=true;
-                    que.offer(new word(wordList.get(i),b+1));
+                String pattern=a.substring(0,i)+'*'+a.substring(i+1);
+                for(String next:map.getOrDefault(pattern,new ArrayList<>()))
+                {
+                if(!visited.contains(next))
+                {
+                    if(next.equals(endWord)) return b+1;
+                    visited.add(next);
+                    que.offer(new Word(next,b+1));
                 }
             }
+        }
         }
         return 0;
     }
