a9f553fe4461333ba5fda8d7dbe08a640a71ffca
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-10-23 21:20:47 +0530

    Time: 3 ms (2.2%), Space: 42.2 MB (42.39%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,77 @@
+class Solution {
+    private int find(int arr[])
+    {
+    int n=arr.length;
+    if (arr[0] <= arr[n-1]||n<2) 
+    {
+        return 0;
+    }
+        int l =0;
+        int r= n-1;
+        while(l<r)
+        {   int mid = l+(r-l)/2;
+            if(arr[mid]>arr[r])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid;
+                
+            }
+        }
+        return l;
+    }
+    private int bs(int arr[],int l,int r,int target)
+    {
+    int n=arr.length;
+    if(n<2)
+    {
+        return 0;
+    }
+
+        while(l<=r)
+        {
+            int mid = l+(r-l)/2;
+            if(arr[mid]==target)
+            {
+                return mid;
+            }
+            else if(arr[mid]>target)
+            {
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+public int search(int arr[], int target) {
+        int f=find(arr);
+        System.out.println(f);
+        if(arr[f]==target)
+        {
+            return f;
+        }
+        int left=bs(arr,0,f,target);
+        int right =bs(arr,f+1,arr.length-1,target);
+        if(left!=-1)
+        {
+            if(arr[left]==target)
+            {
+                return left;
+            }
+        }
+        else if(right!=-1)
+        {
+            if(arr[right]==target)
+            {
+                return right;
+            }
+        }
+        return -1;
+
+    }
+}
