69e23c8831b201869989e4bfa86dd8ed65b903f1
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-11-01 17:06:42 +0530

    Time: 414 ms (5.45%), Space: 237 MB (5.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,32 @@
 class Solution {
+    int dp[][];
+public int recurse(int ind,int prev,int n,int arr[])
+{
+if(ind==n)
+{
+    return 0;
+}
+if(dp[ind][prev+1]!=-1)
+{
+    return dp[ind][prev+1];
+}
+int not_take=0+recurse(ind+1,prev,n,arr);
+int take=0;
+if(prev==-1||arr[ind]>arr[prev])
+{
+    take=1+recurse(ind+1,ind,n,arr);
+}
+dp[ind][prev+1]=Math.max(take,not_take);
+return dp[ind][prev+1];
+}
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        ArrayList<Integer> list=new ArrayList<>();
-        int max=0;
-        for(int num:nums)
-        {
-            int idx=Collections.binarySearch(list,num);
-            if(idx<0)
-            {
-                idx=-idx-1;
-            }
-            if(idx==list.size())
-            {
-                list.add(num);
-            }
-            else 
-            {
-                list.set(idx,num);
-            }
-            max=Math.max(max,list.size());
-             
-        }
-        return max;
         
+        int n =nums.length;
+        dp=new int[n][n+1];
+        for(int ar[]:dp)
+        {
+            Arrays.fill(ar,-1);
+        }
+        return recurse(0,-1,n,nums);
     }
 }
