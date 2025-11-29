8ce1dd5a5ed8ab7a8f7dd6a2b89ff1d3ced6fee9
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-11-29 12:06:49 +0530

    Time: 2 ms (95.87%), Space: 44.8 MB (79.23%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,49 +1,40 @@
 class Solution {
-    static int dp[][]; 
-    public boolean recurse(String s,String p, int i, int j,int n,int m)
-    {
-        if(i==n&&j==m)
-        {
-            dp[i][j]=1;
-            return true;
-        }
-
-        if(j==m)   
-        {
-            dp[i][j]=0;
-            return false;
-        }
-        if(dp[i][j]!=-1) return dp[i][j]==1?true:false;
-        if (i == n) {
-        for (int k = j; k < m; k++) {
-        if (p.charAt(k) != '*') 
-        {
-            dp[i][j]=0;
-        return false;
-        }
-    }
-    dp[i][j]=1;
-    return true;
-}
-boolean ans=false;
-        if(p.charAt(j)=='?'||p.charAt(j)==s.charAt(i))
-        {
-            ans= recurse(s,p,i+1,j+1,n,m);
-        }
-        else if(p.charAt(j)=='*')
-        {
-            ans= recurse(s,p,i+1,j,n,m)||recurse(s,p,i,j+1,n,m);
-                
-        }
-        dp[i][j]=ans?1:0;
-        return ans;
-    }
     public boolean isMatch(String s, String p) {
-        dp=new int[s.length()+1][p.length()+1];
-        for(int arr[]:dp)
+        int i = 0;
+        int j = 0;
+        int n = s.length();
+        int m =p.length();
+        int star=-1;
+        int index=-1;
+        while(i<n)
         {
-            Arrays.fill(arr,-1);
+            if(j < m&&(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?'))
+            {
+                i++;
+                j++;
+            }
+            else if(j < m && p.charAt(j)=='*')
+            {
+                star=j;
+                index=i;
+                j++;
+            }
+            else if(star!=-1)
+            {
+                j=star+1;
+                index++;
+                i=index;
+            }
+            else
+            {
+                return false;
+            }
+
         }
-        return recurse(s,p,0,0,s.length(),p.length());
+        while(j<m&&p.charAt(j)=='*')
+        {
+            j++;
+        }
+        return j==m;
     }
 }
