2e3ed30237b4acf389319b3b71054782aea0206f
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-12-08 23:05:02 +0530

    Time: 40 ms (81.9%), Space: 44.5 MB (24.41%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,9 +1,10 @@
 class Solution {
-    int[][] dp;
-    int[] val;
+    // int[][] dp;
+    // int[] val;
     public int maxCoins(int[] nums) {
-        int n = nums.length;
-        val = new int[n + 2];
+         int n = nums.length;
+
+        int[] val = new int[n + 2];
         val[0] = 1;
         val[n + 1] = 1;
 
@@ -11,26 +12,44 @@ class Solution {
             val[i] = nums[i - 1];
         }
 
-        dp = new int[n + 1][n + 1];
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[n + 2][n + 2];
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j <= n; j++) {
+
+                int best = 0;
+
+                for (int k = i; k <= j; k++) {
+
+                    int coins =
+                        val[i - 1] * val[k] * val[j + 1] +
+                        dp[i][k - 1] +
+                        dp[k + 1][j];
+
+                    best = Math.max(best, coins);
+                }
+
+                dp[i][j] = best;
+            }
         }
-        return recurse(1, n);   
-    }
-    private int recurse(int i, int j) {  
-        if (i > j) return 0;  
 
-        if (dp[i][j] != -1) return dp[i][j];
-        int best = 0;
-        for (int k = i; k <= j; k++) {
-            int coins =
-                val[i - 1] * val[k] * val[j + 1]    
-                + recurse(i, k - 1)                 
-                + recurse(k + 1, j);                 
-
-            best = Math.max(best, coins);
-        }
-
-        return dp[i][j] = best;
+        return dp[1][n];   
     }
 }
+    // private int recurse(int i, int j) {  
+    //     if (i > j) return 0;  
+
+    //     if (dp[i][j] != -1) return dp[i][j];
+    //     int best = 0;
+    //     for (int k = i; k <= j; k++) {
+    //         int coins =
+    //             val[i - 1] * val[k] * val[j + 1]    
+    //             + recurse(i, k - 1)                 
+    //             + recurse(k + 1, j);                 
+
+    //         best = Math.max(best, coins);
+    //     }
+
+    //     return dp[i][j] = best;
+//     }
+// }
