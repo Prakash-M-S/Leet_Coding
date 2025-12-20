93caf47da87a01f2b52fbd31baa8f68588fef4ea
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-12-20 09:58:49 +0530

    Time: 14 ms (90.46%), Space: 44.8 MB (14.08%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,25 +1,25 @@
 class Solution {
-    int dp[][];
-    private int recurse(int i,int j,int[] orig)
-    {
-        if(i>j)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int min =Integer.MAX_VALUE;
-        for(int k =i;k<=j;k++)
-    {
-        int cost=  orig[j+1]-orig[i-1]+recurse(i,k-1,orig)+recurse(k+1,j,orig);
-        min = Math.min(cost,min);
-    }
-    dp[i][j]=min;
-    return min;
+    // int dp[][];
+    // private int recurse(int i,int j,int[] orig)
+    // {
+    //     if(i>j)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1)
+    //     {
+    //         return dp[i][j];
+    //     }
+    //     int min =Integer.MAX_VALUE;
+    //     for(int k =i;k<=j;k++)
+    // {
+    //     int cost=  orig[j+1]-orig[i-1]+recurse(i,k-1,orig)+recurse(k+1,j,orig);
+    //     min = Math.min(cost,min);
+    // }
+    // dp[i][j]=min;
+    // return min;
 
-    }
+    // }
     public int minCost(int n, int[] cuts) {
         int c = cuts.length;
         int stick[]=new int[c+2];
@@ -28,13 +28,23 @@ class Solution {
         {
             stick[i+1] = cuts[i];
         }
-        dp=new int[c+1][c+1];
-        for(int arr[] : dp)
-        {
-            Arrays.fill(arr,-1);
-        }
+        int dp[][]=new int[stick.length][stick.length];
         Arrays.sort(stick);
-        return recurse(1,c,stick);
+        for(int i=c;i>=1;i--)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(i>j) continue;
+                int min=Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++)
+                {
+                    int cost = (stick[j+1] -stick[i-1])+dp[i][k-1]+dp[k+1][j];
+                    min = Math.min(cost,min);
+                }
+                dp[i][j]=min;
+            }
+        }
+        return dp[1][c];
 
     }
 }
