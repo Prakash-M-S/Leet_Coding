2505b48dee3fc923ed01e7fac7a3782fb1268dc2
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2025-12-27 12:14:00 +0530

    Time: 15 ms (7.57%), Space: 73 MB (5.1%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,54 @@
+class Solution {
+    boolean pal[][];
+    List<List<String>> res;
+    private void recurse(int i,String s,int n,List<String> l)
+    {
+        if(i>=n)
+        {
+            res.add(new ArrayList<>(l));
+            return;
+        }
+        String temp="";
+        int minCost = Integer.MAX_VALUE;
+        for(int j =i;j<n;j++)
+        {
+            temp+=s.charAt(j);
+            int cost= 0;
+
+            if(pal[i][j])
+            {
+                l.add(temp);
+                recurse(j+1,s,n,l);
+                l.remove(l.size()-1);
+
+            }
+        }
+        return;
+    }
+    public List<List<String>> partition(String s) {
+        int N =s.length();
+        pal = new boolean[N][N];
+        res = new LinkedList<>();
+        for(int i =N-1;i>=0;i--)
+        {
+            for(int j=i;j<N;j++)
+            {
+                if(i==j)
+                {
+                    pal[i][j]=true;
+                }
+                else if(j==i+1)
+                {
+                    pal[i][j]=(s.charAt(i)==s.charAt(j));
+                }
+                else
+                {
+                    pal[i][j]=(s.charAt(i)==s.charAt(j) && pal[i+1][j-1]);
+                }
+            }
+        }
+        
+        recurse(0,s,N,new ArrayList<>());
+        return res;
+    }
+}
