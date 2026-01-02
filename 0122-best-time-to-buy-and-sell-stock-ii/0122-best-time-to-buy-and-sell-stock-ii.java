1d23749e0e16a2126060fe37f0452f1e187e6179
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2026-01-02 19:47:29 +0530

    Time: 1 ms (17.5%), Space: 46.7 MB (9.43%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,35 +1,38 @@
 class Solution {
-    int dp[][];
-    public int recursion(int i,int n,int holding,int[] prices)
-    {
-        if(i==n)
-        {
-            return 0;
-        }
-        if(dp[i][holding]!=-1)
-        {
-            return dp[i][holding];
-        }
-        if(holding==0)
-        {
-            dp[i][holding]=Math.max(-prices[i]+recursion(i+1,n,1,prices),
-            recursion(i+1,n,0,prices));
-        }
-        else
-        {
-            dp[i][holding]=Math.max(prices[i]+recursion(i+1,n,0,prices),
-            recursion(i+1,n,1,prices));
-        }
-        return dp[i][holding];
-
-    }
+    // int dp[][];
+    // private int recurse(int day,boolean hold,int[] prices,int n)
+    // {
+    //     if(day>=n)
+    //     {
+    //         return 0;
+    //     }
+    //     int max =Integer.MIN_VALUE;
+    //     if(dp[day][hold?1:0]!=-1)
+    //     {
+    //         return dp[day][hold?1:0];
+    //     }
+    //     if(hold)
+    //     {
+    //         int take = +prices[day]+recurse(day+1,false,prices,n);
+    //         int not_take = recurse(day+1,true,prices,n);
+    //         max = Math.max(take,not_take);
+    //     }
+    //     else
+    //     {
+    //         int take = -prices[day]+recurse(day+1,true,prices,n);
+    //         int not_take = recurse(day+1,false,prices,n);
+    //         max = Math.max(take,not_take);
+    //     }
+    //     return dp[day][hold?1:0] = max;
+    // }
     public int maxProfit(int[] prices) {
-        dp=new int[prices.length][2];
-        for(int d[]:dp)
+        int n =prices.length;
+        int[][] dp=new int[n+1][2];
+        for(int day =n-1;day>=0;day--)
         {
-        Arrays.fill(d,-1);
+            dp[day][0]=Math.max(-prices[day]+dp[day+1][1],dp[day+1][0]);
+            dp[day][1]=Math.max(prices[day]+dp[day+1][0],dp[day+1][1]);
         }
-        return recursion(0, prices.length, 0, prices);
-       
+        return dp[0][0];
     }
 }
