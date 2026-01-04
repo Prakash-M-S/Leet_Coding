7712444d06bafa33245fd2ec475fcf9706f56744
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2026-01-04 10:53:25 +0530

    Time: 60 ms (31.21%), Space: 128 MB (24.09%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,43 +1,54 @@
 class Solution {
     int dp[][][];
-    private int recurse(int day,int hold,int trans,int prices[],int n)
-    {
-        if(day==n)
-        {
-            return 0;
-        }
-        if(trans==0)
-        {
-            return 0;
-        }
-        if(dp[day][hold][trans]!=-1)
-        {
-            return dp[day][hold][trans]; 
-        }
-        if(hold ==1)
-        {
-            int sell=prices[day]+recurse(day+1,0,trans-1,prices,n);
-            int not_sell = recurse(day+1,1,trans,prices,n);
-            dp[day][hold][trans]=Math.max(sell,not_sell);
-        }
-        else
-        {
-            int buy = -prices[day]+recurse(day+1,1,trans,prices,n);
-            int not_buy=recurse(day+1,0,trans,prices,n);
-            dp[day][hold][trans]=Math.max(buy,not_buy);
-        }
-        return dp[day][hold][trans];
-    }
+    // private int recurse(int day,int hold,int trans,int prices[],int n)
+    // {
+    //     if(day==n)
+    //     {
+    //         return 0;
+    //     }
+    //     if(trans==0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[day][hold][trans]!=-1)
+    //     {
+    //         return dp[day][hold][trans]; 
+    //     }
+    //     if(hold ==1)
+    //     {
+    //         int sell=prices[day]+recurse(day+1,0,trans-1,prices,n);
+    //         int not_sell = recurse(day+1,1,trans,prices,n);
+    //         dp[day][hold][trans]=Math.max(sell,not_sell);
+    //     }
+    //     else
+    //     {
+    //         int buy = -prices[day]+recurse(day+1,1,trans,prices,n);
+    //         int not_buy=recurse(day+1,0,trans,prices,n);
+    //         dp[day][hold][trans]=Math.max(buy,not_buy);
+    //     }
+    //     return dp[day][hold][trans];
+    // }
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        dp=new int[n][2][3];
-        for(int arr[][]:dp)
+        dp=new int[n+1][2][3];
+        
+        // for(int arr[][]:dp)
+        // {
+        //     for(int a[]:arr)
+        //     {
+        //         Arrays.fill(a,-1);
+        //     }
+        // }
+        // return recurse(0,0,2,prices,n);
+        for(int day = n-1;day>=0;day--)
         {
-            for(int a[]:arr)
+            for(int trans = 1;trans<=2;trans++)
             {
-                Arrays.fill(a,-1);
+                dp[day][0][trans]=Math.max(-prices[day]+dp[day+1][1][trans],dp[day+1][0][trans]);
+                dp[day][1][trans]=Math.max(prices[day]+dp[day+1][0][trans-1],dp[day+1][1][trans]);
             }
         }
-        return recurse(0,0,2,prices,n);
+    return dp[0][0][2];
     }
+
 }
