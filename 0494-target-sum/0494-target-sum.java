d435631e33077aa34bc1d85d175f1ceb7047ed2f
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2026-01-11 09:56:56 +0530

    Time: 15 ms (47.55%), Space: 47 MB (17.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,31 +1,51 @@
 class Solution {
     int[][] dp;
-    int totalSum;
 
-    private int recurse(int i, int sum, int[] nums, int target) {
-        if (i == nums.length) {
-            return sum == target ? 1 : 0;
-        }
-
-        int idx = sum + totalSum;
-        if (dp[i][idx] != -1)
-            return dp[i][idx];
-
-        int add = recurse(i + 1, sum + nums[i], nums, target);
-        int sub = recurse(i + 1, sum - nums[i], nums, target);
-
-        return dp[i][idx] = add + sub;
-    }
+    // private int recurse(int i, int sum,int[] nums,int target) {
+    //     if(sum==target&&i==nums.length)
+    //     {
+    //         return 1;
+    //     }
+    //     else if(i==nums.length) return 0;
+    //     int idx=totalSum+sum;
+    //     if(dp[i][idx]!=-1) return dp[i][idx];
+        
+    //     int add=recurse(i+1,sum+nums[i],nums,target);
+    //     int sub=recurse(i+1,sum-nums[i],nums,target);
+    //     return dp[i][idx]=add+sub;
+    // }
 
     public int findTargetSumWays(int[] nums, int target) {
-        totalSum = 0;
-        for (int x : nums) totalSum += x;
+        int n =nums.length;
+        int totalSum=0;
 
-        dp = new int[nums.length][2 * totalSum + 1];
-        for (int i = 0; i < nums.length; i++)
-            Arrays.fill(dp[i], -1);
-
-        return recurse(0, 0, nums, target);
+        for(int i=0;i<n;i++) totalSum+=nums[i];
+        int[][] dp=new int[n+1][(2*totalSum)+1];
+        // for (int i =0;i<nums.length;i++)
+        //     Arrays.fill(dp[i], -1);
+        // return recurse(0, 0, nums,target);
+        if (Math.abs(target) > totalSum) return 0;
+        dp[n][target+totalSum]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int s=-totalSum;s<=totalSum;s++)
+            {
+                int idx=s+totalSum;
+                int ways=0;
+                int add=s+nums[i];
+                int sub=s-nums[i];
+                if(add>=-totalSum&&add<=totalSum)
+                {
+                    ways+=dp[i+1][add+totalSum];
+                }
+                if(sub>=-totalSum&&sub<=totalSum)
+                {
+                    ways+=dp[i+1][sub+totalSum];
+                }
+                dp[i][idx]=ways
+;            }
+        }
+        return dp[0][totalSum];
     }
 }
 
