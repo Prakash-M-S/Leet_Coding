515e4aedf8ec1eba0bf248e05965863b215869f1
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2026-01-12 11:55:48 +0530

    Time: 54 ms (43.48%), Space: 123 MB (6.9%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,13 +1,14 @@
 class Solution {
     Boolean dp[][];
     int tot;
+    int need;
     private boolean recurse(int idx,int sum,int nums[],int n)
     {
-        if(sum==(tot-sum))
+        if(sum==need)
         {
             return true;
         }
-        if(idx==n)
+        if(idx==n||sum>tot)
         {
             return false;
         }
@@ -15,14 +16,16 @@ class Solution {
         {
             return dp[idx][sum];
         }
-        dp[idx][sum]=recurse(idx+1,sum+nums[idx],nums,n)|recurse(idx+1,sum,nums,n);
+        dp[idx][sum]=recurse(idx+1,sum+nums[idx],nums,n)||recurse(idx+1,sum,nums,n);
         return dp[idx][sum];
     }
     public boolean canPartition(int[] nums) {
      tot=0;
         int n =nums.length;
         for(int a:nums) tot+=a;
+        if((tot&1)!=0) return false;
         dp=new Boolean[n][tot+1];
+        need = tot/2;
         return recurse(0,0,nums,n);
     }
 }
