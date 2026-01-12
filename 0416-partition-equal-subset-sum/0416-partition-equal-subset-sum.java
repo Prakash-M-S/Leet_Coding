453b254acfc4e21cd704b8f4f84b9fac648c78f7
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2026-01-12 14:39:40 +0530

    Time: 31 ms (79.75%), Space: 43.9 MB (85.45%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,31 +1,45 @@
 class Solution {
-    Boolean dp[][];
-    int tot;
-    int need;
-    private boolean recurse(int idx,int sum,int nums[],int n)
-    {
-        if(sum==need)
-        {
-            return true;
-        }
-        if(idx==n||sum>tot)
-        {
-            return false;
-        }
-        if(dp[idx][sum]!=null)
-        {
-            return dp[idx][sum];
-        }
-        dp[idx][sum]=recurse(idx+1,sum+nums[idx],nums,n)||recurse(idx+1,sum,nums,n);
-        return dp[idx][sum];
-    }
+    // Boolean dp[][];
+    // int tot;
+    // int need;
+    // private boolean recurse(int idx,int sum,int nums[],int n)
+    // {
+    //     if(sum==need)
+    //     {
+    //         return true;
+    //     }
+    //     if(idx==n||sum>need)
+    //     {
+    //         return false;
+    //     }
+    //     if(dp[idx][sum]!=null)
+    //     {
+    //         return dp[idx][sum];
+    //     }
+    //     dp[idx][sum]=recurse(idx+1,sum+nums[idx],nums,n)||recurse(idx+1,sum,nums,n);
+    //     return dp[idx][sum];
+    // }
     public boolean canPartition(int[] nums) {
-     tot=0;
+        int tot=0;
         int n =nums.length;
         for(int a:nums) tot+=a;
         if((tot&1)!=0) return false;
-        dp=new Boolean[n][tot+1];
-        need = tot/2;
-        return recurse(0,0,nums,n);
-    }
-}
+        int need = tot/2;
+        boolean dp[]=new boolean[need+1];
+        dp[0]=true;
+        // boolean dp[][]=new boolean[n+1][need+1];
+        // dp[n][need]=true;
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int sum=need;sum>=nums[idx];sum--)
+            {
+                int curr = sum-nums[idx];
+                dp[sum]=dp[sum]|dp[curr];
+
+            }
+            }
+            return dp[need];
+        }
+        }
+        // return recurse(0,0,nums,n);
+    
