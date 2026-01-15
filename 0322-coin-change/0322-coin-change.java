3ff47fe0d9efbcb806f0b686c2292e087285292f
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2026-01-15 12:36:26 +0530

    Time: 14 ms (87.98%), Space: 46.4 MB (53.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,29 +2,23 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n =coins.length;
         int INF = (int)1e9;
-        int dp[][]=new int[n+1][amount+1];
-        for(int a[]:dp)
-        {
-            Arrays.fill(a,INF);
-        }
-        for(int i=0;i<=n;i++) dp[i][0]=0;
+        int dp[]=new int[amount+1];
 
+            Arrays.fill(dp,INF);
+        dp[0]=0;
         // Arrays.sort(coins);
         for(int i=1;i<=n;i++)
         {
-            for(int amt=1;amt<=amount;amt++)
+            for(int amt=coins[i-1];amt<=amount;amt++)
             {
-
-                    dp[i][amt]=dp[i-1][amt];
                  
-                if (coins[i - 1] <= amt && dp[i][amt - coins[i - 1]] != INF) {
-                    dp[i][amt] =
-                        Math.min(dp[i][amt],
-                                 1 + dp[i][amt - coins[i - 1]]);
-                }
+                    dp[amt] =
+                        Math.min(dp[amt],
+                                 1 + dp[amt - coins[i - 1]]);
+                
             }
         }
-        return dp[n][amount]==INF?-1:dp[n][amount];
+        return dp[amount]==INF?-1:dp[amount];
 
     }
 }
