08a21f5901af9e4c5774c270a173eab970885e49
Prakash M S <168627798+Prakash-M-S@users.noreply.github.com>
2026-01-24 21:51:07 +0530

    Time: 1 ms (60.33%), Space: 48.3 MB (36.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,33 @@
 import java.util.*;
 class Solution {
     public int trap(int[] arr) {
-        int n = arr.length;
-        if (n == 0) return 0; 
-        int[] maxl = new int[n];
-        int[] maxr = new int[n];
-        maxl[0] = 0; 
-        for (int i = 1; i < n; i++) {
-            maxl[i] = Math.max(maxl[i - 1], arr[i - 1]);
-        }
-        maxr[n - 1] = 0; 
-        for (int i = n - 2; i >= 0; i--) {
-            maxr[i] = Math.max(maxr[i + 1], arr[i + 1]);
-        }
+        int n=arr.length;
         int sum=0;
-        for(int i=0;i<n;i++)
+        if(n==0)
         {
-            int val=Math.min(maxl[i],maxr[i])-arr[i];
-            if(val<0) val=0;
-sum+=val;
+            return sum;
         }
-        return sum;
+        int left[]=new int[n];
+        int right[]=new int[n];
+        
+        right[n-1]=0;
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=Math.max(right[i+1],arr[i+1]);
+        }
+        left[0]=0;
+        for(int i=1;i<n;i++)
+        {
+            left[i]=Math.max(left[i-1],arr[i-1]);
+        }
+        for(int i=0;i<n;i++)
+         {
+        int val=Math.min(left[i],right[i])-arr[i];
+        if(val<0) val=0;
+        
+        sum+=val;
+        }
+    return sum;
 
     }
 }
